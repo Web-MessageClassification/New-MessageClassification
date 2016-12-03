bf27a96d9121df8b1927d75ebb7c1ca9edb4ff37
lilelr <12211104lilele>
2016-12-03 10:34:13 +0800

    修改正确率，召回率，计算代码，请大家相应更改

--- logistic_regression.py
+++ logistic_regression.py
@@ -7,6 +7,8 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 # import the LogisticRegression
 from sklearn.linear_model import LogisticRegression
+from sklearn.externals import joblib
+from time import clock
 import var
 
 
@@ -48,6 +50,7 @@ def makecrossvaliddata(datamat, labelmat, it, k):
             label.append(labelmat[i])
     return data, label, validdata, validlabel
 
+start = clock()
 # 载入训练集
 labelmat, datamat = loadtrain(var.train_data_path, maxline=800000)
 
@@ -59,7 +62,24 @@ datamat = cutdata(datamat)
 # testdataMat = cutdata(testdataMat)
 
 # 将字符串标签转换为整型
-labelmat = [-1 if int(x) == 0 else 1 for x in labelmat]
+labelmat = [0 if int(x) == 0 else 1 for x in labelmat]
+
+#
+# word_count_vect = CountVectorizer()
+# # joblib.dump(word_count_vect,"word_count_vect.m")
+# X_train_counts = word_count_vect.fit_transform(datamat)
+
+# 训练数据计算tf-idf矩阵，并训练出Logistc_regression模型
+# tfidf_transformer = TfidfTransformer()
+# joblib.dump(tfidf_transformer,"tfidf_transformer")
+# X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
+# # 训练出回归系数
+# start = clock()
+# clf = LogisticRegression()
+# clf.fit(X_train_tfidf, labelmat)
+# joblib.dump(clf,"lr_model.m")
+# finsh = clock()
+# print (finsh-start)
 
 # 交叉验证的步数
 corssvalid_k = 5
@@ -79,12 +99,9 @@ for it in range(corssvalid_k):
     tfidf_transformer = TfidfTransformer()
     X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
 
-    # # 实例一个SVM分类器
-    # # TODO 自己实现
-    # clf = SGDClassifier(loss='hinge', alpha=1e-3, n_iter=5, random_state=42).fit(X_train_tfidf, labelmat)
 
     # 实现Logistic Regression 分类器
-    clf = LogisticRegression(penalty='l1')
+    clf = LogisticRegression()
     clf.fit(X_train_tfidf, labelmat)
 
     # 计算新数据tfidf
@@ -93,16 +110,34 @@ for it in range(corssvalid_k):
 
     # 判别新数据
     predicted = clf.predict(X_new_tfidf)
-
-    # for i in range(len(predicted)):
-    #     print('%d\t%s' % (predicted[i], validdatamat[i]))
-
     # 计算正确率, 召回率和F1-Measure
-    corssvalid_q[it] = np.mean(predicted == validlabelmat)
-    corssvalid_recall[it] = 1.0 * len(validlabelmat) / len(labelmat) * np.mean(predicted == validlabelmat)
+    pre_accurate =0.0
+    recall_accurate = 0.0
+    sum_acc = 0.0
+    sum_re = 0.0
+    for i in range(len(predicted)):
+        tmp_predit = int(predicted[i])
+        tmp_valid= int(validlabelmat[i])
+        # print('%d\t%s' % (predicted[i], validdatamat[i]))
+        if tmp_predit == 1 and tmp_predit==tmp_valid:
+            pre_accurate+=1
+        if tmp_valid == 1 and tmp_valid == tmp_predit:
+            recall_accurate += 1
+
+        if tmp_predit ==1:
+            sum_acc+=1
+        if tmp_valid == 1:
+            sum_re+=1
+    # 计算正确率
+    corssvalid_q[it] = pre_accurate/sum_acc
+    # 计算召回率
+    corssvalid_recall[it] = recall_accurate/sum_re
+    # 计算F1-Measure
     F1[it] = 2.0 * corssvalid_q[it] * corssvalid_recall[it] / (corssvalid_q[it] + corssvalid_recall[it])
 
 
 print 'Precision Rate: ',np.mean(corssvalid_q)
 print 'Recall Rate:    ',np.mean(corssvalid_recall)
 print 'F1-Measure:     ',np.mean(F1)
+finish = clock()
+print '所用时间：'+str(finish - start)
